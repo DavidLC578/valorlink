d201036510a83bd928a5a6b8c7ad0e414c9eea17
DavidLC578 <davidlatt2002@gmail.com>
2025-08-07 12:39:07 +0200

    feat: middleware for non authenticated users, and authenticated users

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,41 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
-export const config = { matcher: ["/home/:path*", "/profile/:path*"] }
+// Array of protected routes
+const protectedRoutes = [
+    '/home',
+    '/profile',
+];
+
+export async function middleware(req) {
+    const { pathname } = req.nextUrl;
+    const token = await getToken({ req });
+
+    // Redirect to /home if the user is authenticated and is at the root
+    if (pathname === '/') {
+        if (token) {
+            return NextResponse.redirect(new URL('/home', req.url));
+        }
+        return NextResponse.next();
+    }
+
+    // Verify protected routes
+    const isProtectedRoute = protectedRoutes.some(route =>
+        pathname === route || pathname.startsWith(`${route}/`)
+    );
+
+    // Redirect to /auth/signin if the user is not authenticated and is at a protected route
+    if (isProtectedRoute && !token) {
+        return NextResponse.redirect(new URL('/auth/signin', req.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        '/',
+        '/home/:path*',
+        '/profile/:path*',
+    ]
+};
