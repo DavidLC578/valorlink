bbcfd5f89dd0dda3155a04c1a748f814cf8cabb3
DavidLC578 <davidlatt2002@gmail.com>
2025-08-07 12:00:31 +0200

    remove console logs

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -130,7 +130,6 @@ function ProfileComponent() {
                                                     className="text-white text-2xl font-bold bg-transparent border-none focus:outline-none cursor-pointer w-full"
                                                     defaultValue={playerInfo?.player.Player?.availability || 'Available'}
                                                     onChange={async (e) => {
-                                                        console.log('Availability changed to:', e.target.value)
                                                         const response = await fetch(`/api/profile/${playerInfo?.player.username}`, {
                                                             method: 'PUT',
                                                             headers: {
@@ -139,7 +138,6 @@ function ProfileComponent() {
                                                             body: JSON.stringify({ availability: e.target.value }),
                                                         });
                                                         const data = await response.json();
-                                                        console.log(data);
                                                     }}
                                                 >
                                                     <option className="text-white bg-slate-800" value="Available">Available</option>
