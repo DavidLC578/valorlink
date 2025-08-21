5af89ffe6584f9ddcd521dfdff512dfe384bbf64
DavidLC578 <davidlatt2002@gmail.com>
2025-08-21 11:33:48 +0200

    feat: add get friends endpoint route

---
+++ src/app/api/friends/route.js
@@ -0,0 +1,59 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import db from "@/libs/db";
+
+export async function GET() {
+    const session = await getServerSession(authOptions)
+    if (!session?.user?.id) {
+        return NextResponse.json(
+            { message: "Unauthorized: Please log in to continue" },
+            { status: 401 }
+        );
+    }
+
+    try {
+        const friendships = await db.friendship.findMany({
+            where: {
+                OR: [
+                    { senderId: session.user.id, status: "ACCEPTED" },
+                    { receiverId: session.user.id, status: "ACCEPTED" }
+                ]
+            },
+            include: {
+                sender: {
+                    select: {
+                        Player: true
+                    }
+                },
+                receiver: {
+                    select: {
+                        Player: true
+                    }
+                }
+            }
+        })
+
+        console.log(friendships)
+
+        const friends = friendships.map(friendship => ({
+            ...(friendship.senderId === session.user.id ? friendship.receiver : friendship.sender),
+            friendshipId: friendship.id
+        }));
+
+        return NextResponse.json(friends, { status: 200 });
+    } catch (error) {
+        console.error("Error fetching friends:", error);
+        return NextResponse.json(
+            { message: "Internal Server Error" },
+            { status: 500 }
+
+        );
+
+    }
+
+
+
+
+
+}
